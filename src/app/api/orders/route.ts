3b9c075b1f2fdefea946ebb61bc0eaac637a867f
Habib ur Rehman <habibaeo123@gmail.com>
2023-04-23 20:32:11 +0500

    updated orders variables

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -3,28 +3,28 @@ import { Pool } from '@neondatabase/serverless';
 interface Order {
   id: string;
   bookId: string;
-  clientName: string;
-  status: string;
+  customerName: string;
+  
 }
 
 export async function handleRequest(request: Request): Promise<Response> {
   const pool = new Pool({ connectionString: process.env.NEON_DATABASE_URL });
   const requestData = await request.json();
 
-  const { bookId, clientName } = requestData;
-  if (!bookId || !clientName) {
+  const { bookId, customerName } = requestData;
+  if (!bookId || !customerName) {
     return new Response(JSON.stringify({ "error": "Invalid request parameters" }), { status: 400 });
   }
 
   const clientEmail = requestData.clientEmail || '';
 
   try {
-    const { rows } = await pool.query(`INSERT INTO orders (book_id, client_name, client_email) VALUES ($1, $2, $3) RETURNING *`, [bookId, clientName, clientEmail]);
+    const { rows } = await pool.query(`INSERT INTO orders (book_id, client_name, client_email) VALUES ($1, $2, $3) RETURNING *`, [bookId, customerName, clientEmail]);
     const order: Order = {
       id: rows[0].id,
-      bookId: rows[0].book_id,
-      clientName: rows[0].client_name,
-      status: rows[0].status
+      bookId: rows[0].bookId,
+      customerName: rows[0].customerName,
+   
     };
 
     const content = JSON.stringify(order);
