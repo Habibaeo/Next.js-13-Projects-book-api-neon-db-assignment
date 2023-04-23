cd6a3569fd3f0443d85a9a7dbb694c68d1111722
Habib ur Rehman <habibaeo123@gmail.com>
2023-04-23 20:31:27 +0500

    updated order columns

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -7,29 +7,33 @@ interface Order {
   status: string;
 }
 
-export async function POST(request: Request) {
+export async function handleRequest(request: Request): Promise<Response> {
   const pool = new Pool({ connectionString: process.env.NEON_DATABASE_URL });
   const requestData = await request.json();
-  const { bookId, clientName } = requestData;
 
+  const { bookId, clientName } = requestData;
   if (!bookId || !clientName) {
     return new Response(JSON.stringify({ "error": "Invalid request parameters" }), { status: 400 });
   }
 
   const clientEmail = requestData.clientEmail || '';
 
-  const { rows } = await pool.query(`INSERT INTO orders (book_id, client_name, client_email) VALUES ('${bookId}', '${clientName}', '${clientEmail}') RETURNING *`);
-  const order: Order = {
-    id: rows[0].id,
-    bookId: rows[0].book_id,
-    clientName: rows[0].client_name,
-    status: rows[0].status
-  };
+  try {
+    const { rows } = await pool.query(`INSERT INTO orders (book_id, client_name, client_email) VALUES ($1, $2, $3) RETURNING *`, [bookId, clientName, clientEmail]);
+    const order: Order = {
+      id: rows[0].id,
+      bookId: rows[0].book_id,
+      clientName: rows[0].client_name,
+      status: rows[0].status
+    };
 
-  const content = JSON.stringify(order);
-
-  // event.waitUntil(pool.end());  // doesn't hold up the response
-  return new Response(content);
+    const content = JSON.stringify(order);
+    return new Response(content);
+  } catch (error) {
+    return new Response(JSON.stringify({ "error": "Internal Server Error" }), { status: 500 });
+  } finally {
+    await pool.end();
+  }
 }
 
 export const runtime = 'edge';
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
           width={180}
           height={37}
           priority
-        />
+        /> Test Preview
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
