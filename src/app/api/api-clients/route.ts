67efbe47b99baba453a4cb6c422bdfe0365e2ae1
Habib ur Rehman <habibaeo123@gmail.com>
2023-04-24 22:52:31 +0500

    jwt implementation

--- src/app/api/api-clients/route.ts
+++ src/app/api/api-clients/route.ts
@@ -1,20 +1,25 @@
 import { Pool } from '@neondatabase/serverless';
+import jwt from 'jsonwebtoken';
+
 
 interface ApiClient {
   id: number;
   clientName: string;
   clientEmail: string;
+  accessPermissions: string[];
 }
 
 interface ApiClientToken {
   id: number;
   apiClientId: number;
   accessToken: string;
+  jwtToken: string; // add a field for JWT token
 }
 
+// ...
+
 export async function POST(request: Request) {
-  console.log(`My API request: Trying to Post ${JSON.stringify(request)}`)
-    const { clientName, clientEmail }:ApiClient = await request.json();
+  const { clientName, clientEmail }: ApiClient = await request.json();
 
   const pool = new Pool({ connectionString: process.env.NEON_DATABASE_URL });
 
@@ -25,10 +30,14 @@ export async function POST(request: Request) {
   );
 
   // Generate and insert new api_client_token row
-  const accessToken = Math.random().toString(36).substring(7);
+  const jwtToken = jwt.sign(
+    { apiClientId: apiClient.id, clientName, clientEmail, accessPermissions: ['/orders'] },
+    process.env.JWT_SECRET as string, // use your JWT secret here
+    { expiresIn: '1h' } // optional: set token expiration time
+  );
   const { rows: [apiClientToken] } = await pool.query(
-    `INSERT INTO api_client_tokens (apiClientId, accessToken) VALUES ($1, $2) RETURNING *`,
-    [apiClient.id, accessToken],
+    `INSERT INTO api_client_tokens (apiClientId, accessToken, jwtToken) VALUES ($1, $2, $3) RETURNING *`,
+    [apiClient.id, '', jwtToken],
   );
 
   const responseBody = JSON.stringify({
@@ -42,6 +51,54 @@ export async function POST(request: Request) {
   return new Response(responseBody);
 }
 
-//  export const methods = ['POST'];
-// export const next = '/api-clients';
-export const runtime = 'edge';
+
+
+
+
+
+
+
+// interface ApiClient {
+//   id: number;
+//   clientName: string;
+//   clientEmail: string;
+// }
+
+// interface ApiClientToken {
+//   id: number;
+//   apiClientId: number;
+//   accessToken: string;
+// }
+
+// export async function POST(request: Request) {
+//   console.log(`My API request: Trying to Post ${JSON.stringify(request)}`)
+//     const { clientName, clientEmail }:ApiClient = await request.json();
+
+//   const pool = new Pool({ connectionString: process.env.NEON_DATABASE_URL });
+
+//   // Insert new api_client row
+//   const { rows: [apiClient] } = await pool.query(
+//     `INSERT INTO api_clients (clientName, clientEmail) VALUES ($1, $2) RETURNING *`,
+//     [clientName, clientEmail],
+//   );
+
+//   // Generate and insert new api_client_token row
+//   const accessToken = Math.random().toString(36).substring(7);
+//   const { rows: [apiClientToken] } = await pool.query(
+//     `INSERT INTO api_client_tokens (apiClientId, accessToken) VALUES ($1, $2) RETURNING *`,
+//     [apiClient.id, accessToken],
+//   );
+
+//   const responseBody = JSON.stringify({
+//     message: "API client and token successfully created",
+//     data: {
+//       apiClient,
+//       apiClientToken,
+//     },
+//   });
+
+//   return new Response(responseBody);
+// }
+
+
+// export const runtime = 'edge';
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,81 +1,165 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-
 import { jwtVerify } from 'jose';
 
-const allowedAccessTypes:string[] = ['/order'];
+const allowedAccessTypes: string[] = ['/orders'];
 
 async function verify(token: string, secret: string): Promise<any> {
-    //{ apiClientId: string, clientName: string, clientEmail: string, accessPermissions: string }
-    const {payload} = await jwtVerify(token, new TextEncoder().encode(secret));
-    // run some checks on the returned payload, perhaps you expect some specific values
-    console.log('in verify: ', JSON.stringify(payload));
-    // if its all good, return it, or perhaps just return a boolean
-    return payload;
+  const { payload } = await jwtVerify(token, new TextEncoder().encode(secret));
+  console.log('in verify: ', JSON.stringify(payload));
+  return payload;
 }
 
-// This function can be marked `async` if using `await` inside
-export async function middleware(request: NextRequest, ) {
-    let jwtAuthToken = request.headers.get('Authorization');
-    console.log(jwtAuthToken);
-    let response: NextResponse;
-    if (!jwtAuthToken) {
-        // Respond with JSON indicating an error message
-        return new NextResponse(
-                JSON.stringify({ success: false, message: 'authentication failed - no Bearer token provided' }),
-                { status: 401, headers: { 'content-type': 'application/json' } }
-        );
-    } else { 
+export async function middleware(request: NextRequest) {
+  const jwtAuthToken = request.headers.get('Authorization');
+  console.log(jwtAuthToken);
+  let response: NextResponse;
 
-        let decodedToken: any;
-        if (jwtAuthToken.toLowerCase().startsWith('bearer')) {
-            jwtAuthToken = jwtAuthToken.slice('bearer'.length).trim();
-            console.log('jwtAuthToken:', jwtAuthToken);
-            try {
-                decodedToken = await verify(jwtAuthToken, process.env.JWT_SECRET as string);
-                console.log('decoded:',decodedToken);
-            } catch (e:any) {
-                console.log(e.message);
-                return new NextResponse(
-                    JSON.stringify({ success: false, message: 'invalid token provided' }),
-                    { status: 401, headers: { 'content-type': 'application/json' } }
-                );
-            }
+  if (!jwtAuthToken) {
+    return new NextResponse(
+      JSON.stringify({ success: false, message: 'Authentication failed - no Bearer token provided' }),
+      { status: 401, headers: { 'content-type': 'application/json' } }
+    );
+  }
 
-            // const hasAccessToEndpoint = config.matcher.some(
-            //     (at: string) => decodedToken.accessTypes.some((uat:string) => uat === at)
-            // );
-            
-            // if (!hasAccessToEndpoint) {
-            //     return new NextResponse(
-            //         JSON.stringify({ success: false, message: 'not enough privileges to access endpoint' }),
-            //         { status: 401, headers: { 'content-type': 'application/json' } }
-            //     );
-            // }
-            // Clone the request headers and set a new header `x-hello-from-middleware1`
-
-            const requestHeaders = new Headers(request.headers)
-            requestHeaders.set('x-api-client-id-from-middleware', decodedToken.apiClientId);
-
-            // You can also set request headers in NextResponse.rewrite
-            response = NextResponse.next({
-                request: {
-                    // New request headers
-                    headers: requestHeaders,
-                },
-            });
-        } else {
-            return new NextResponse(
-                JSON.stringify({ success: false, message: 'authentication failed - malformed Bearer token provided' }),
-                { status: 401, headers: { 'content-type': 'application/json' } }
-            );
-        }
+  let decodedToken: any;
+  if (jwtAuthToken.toLowerCase().startsWith('bearer')) {
+    const token = jwtAuthToken.slice('bearer'.length).trim();
+    console.log('jwtAuthToken:', token);
+    try {
+      decodedToken = await verify(token, process.env.JWT_SECRET as string);
+      console.log('decoded:', decodedToken);
+    } catch (e: any) {
+      console.log(e.message);
+      return new NextResponse(
+        JSON.stringify({ success: false, message: 'Invalid token provided' }),
+        { status: 401, headers: { 'content-type': 'application/json' } }
+      );
     }
 
-    return response;
+    const hasAccessToEndpoint = allowedAccessTypes.some(at => decodedToken.accessPermissions.includes(at));
+    if (!hasAccessToEndpoint) {
+      return new NextResponse(
+        JSON.stringify({ success: false, message: 'Not enough privileges to access endpoint' }),
+        { status: 401, headers: { 'content-type': 'application/json' } }
+      );
+    }
+
+    const requestHeaders = new Headers(request.headers);
+    requestHeaders.set('x-api-client-id-from-middleware', decodedToken.apiClientId);
+
+    response = NextResponse.next({
+      request: {
+        headers: requestHeaders,
+      },
+    });
+  } else {
+    return new NextResponse(
+      JSON.stringify({ success: false, message: 'Authentication failed - malformed Bearer token provided' }),
+      { status: 401, headers: { 'content-type': 'application/json' } }
+    );
+  }
+
+  return response;
 }
 
-// See "Matching Paths" below to learn more
+// export const config = {
+//   api: {
+//     bodyParser: {
+//       sizeLimit: '1mb',
+//     },
+//   },
+// }
+
 export const config = {
-    matcher: ['/orde', '/orde/:id*']
+    matcher: ['/orders', '/orders/:id*']
 }
+
+
+
+
+
+
+
+
+// import { NextResponse } from 'next/server'
+// import type { NextRequest } from 'next/server'
+
+// import { jwtVerify } from 'jose';
+
+// const allowedAccessTypes:string[] = ['/orders'];
+
+// async function verify(token: string, secret: string): Promise<any> {
+//     //{ apiClientId: string, clientName: string, clientEmail: string, accessPermissions: string }
+//     const {payload} = await jwtVerify(token, new TextEncoder().encode(secret));
+//     // run some checks on the returned payload, perhaps you expect some specific values
+//     console.log('in verify: ', JSON.stringify(payload));
+//     // if its all good, return it, or perhaps just return a boolean
+//     return payload;
+// }
+
+// // This function can be marked `async` if using `await` inside
+// export async function middleware(request: NextRequest, ) {
+//     let jwtAuthToken = request.headers.get('Authorization');
+//     console.log(jwtAuthToken);
+//     let response: NextResponse;
+//     if (!jwtAuthToken) {
+//         // Respond with JSON indicating an error message
+//         return new NextResponse(
+//                 JSON.stringify({ success: false, message: 'authentication failed - no Bearer token provided' }),
+//                 { status: 401, headers: { 'content-type': 'application/json' } }
+//         );
+//     } else { 
+
+//         let decodedToken: any;
+//         if (jwtAuthToken.toLowerCase().startsWith('bearer')) {
+//             jwtAuthToken = jwtAuthToken.slice('bearer'.length).trim();
+//             console.log('jwtAuthToken:', jwtAuthToken);
+//             try {
+//                 decodedToken = await verify(jwtAuthToken, process.env.JWT_SECRET as string);
+//                 console.log('decoded:',decodedToken);
+//             } catch (e:any) {
+//                 console.log(e.message);
+//                 return new NextResponse(
+//                     JSON.stringify({ success: false, message: 'invalid token provided' }),
+//                     { status: 401, headers: { 'content-type': 'application/json' } }
+//                 );
+//             }
+
+//             // const hasAccessToEndpoint = config.matcher.some(
+//             //     (at: string) => decodedToken.accessTypes.some((uat:string) => uat === at)
+//             // );
+            
+//             // if (!hasAccessToEndpoint) {
+//             //     return new NextResponse(
+//             //         JSON.stringify({ success: false, message: 'not enough privileges to access endpoint' }),
+//             //         { status: 401, headers: { 'content-type': 'application/json' } }
+//             //     );
+//             // }
+//             // Clone the request headers and set a new header `x-hello-from-middleware1`
+
+//             const requestHeaders = new Headers(request.headers)
+//             requestHeaders.set('x-api-client-id-from-middleware', decodedToken.apiClientId);
+
+//             // You can also set request headers in NextResponse.rewrite
+//             response = NextResponse.next({
+//                 request: {
+//                     // New request headers
+//                     headers: requestHeaders,
+//                 },
+//             });
+//         } else {
+//             return new NextResponse(
+//                 JSON.stringify({ success: false, message: 'authentication failed - malformed Bearer token provided' }),
+//                 { status: 401, headers: { 'content-type': 'application/json' } }
+//             );
+//         }
+//     }
+
+//     return response;
+// }
+
+// // See "Matching Paths" below to learn more
+// export const config = {
+//     matcher: ['/orders', '/orders/:id*']
+// }
