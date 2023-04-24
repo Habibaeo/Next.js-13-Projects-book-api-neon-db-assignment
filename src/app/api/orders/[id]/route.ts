b3af5b69a98eddeaa24fabcbeab60e2190d0e293
Habib ur Rehman <habibaeo123@gmail.com>
2023-04-24 20:26:58 +0500

    delete request updated

--- src/app/api/orders/[id]/route.ts
+++ src/app/api/orders/[id]/route.ts
@@ -5,15 +5,14 @@ export async function DELETE(request: NextRequest, { params }: { params: { order
   const orderId = params.orderId;
   const pool = new Pool({ connectionString: process.env.NEON_DATABASE_URL });
 
-  try {
-    const result = await pool.query('DELETE FROM orders WHERE id=$1', [orderId]);
-    if (result.rowCount === 0) {
-      return new NextResponse(JSON.stringify({ error: `Order with id ${orderId} not found` }), { status: 404 });
-    }
-    return new NextResponse(JSON.stringify({ message: `Order with id ${orderId} deleted successfully` }));
-  } catch (error:any) {
-    return new NextResponse(JSON.stringify({ error: error.message }), { status: 500 });
+  const result = await pool.query('DELETE FROM orders WHERE id=$1 RETURNING *', [orderId]);
+  const deletedOrder = result.rows[0];
+
+  if (!deletedOrder) {
+    return new NextResponse(JSON.stringify({ error: `Order with id ${orderId} not found` }), { status: 404 });
   }
+
+  return new NextResponse(JSON.stringify({ message: `Order with id ${orderId} successfully deleted` }));
 }
 
 export const runtime = 'edge';
