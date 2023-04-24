458edca361a2b450460ef68175bfc4e44dcb511e
Habib ur Rehman <habibaeo123@gmail.com>
2023-04-24 09:46:22 +0500

    url updated

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
     PGUSER: "habibaeo123",
     PGPASSWORD: "vNcWxo0dUYk6",
     NEON_DATABASE_URL:
-      "postgres://habibaeo123:GBkqX95CmENF@ep-lively-resonance-748842.us-east-2.aws.neon.tech/neondb",
+      "postgres://habibaeo123:vNcWxo0dUYk6@ep-lively-resonance-748842.us-east-2.aws.neon.tech/neondb",
   },
 };
 
