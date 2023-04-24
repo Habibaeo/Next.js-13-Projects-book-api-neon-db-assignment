16315c06537f1ff2f4edf792972b44acbbe9db90
Habib ur Rehman <habibaeo123@gmail.com>
2023-04-24 21:43:54 +0500

    status updated

--- src/app/api/status/route.ts
+++ src/app/api/status/route.ts
@@ -1,18 +1,56 @@
 import { NextResponse, NextRequest } from 'next/server';
 import { Pool } from '@neondatabase/serverless';
 
+// async function checkApiAvailability() {
+//   const pool = new Pool({ connectionString: process.env.NEON_DATABASE_URL });
 
-export async function checkApiAvailability() {
+//   try {
+//     const { rows } = await pool.query('SELECT NOW()');
+//     return true;
+//   } catch (error) {
+//     console.error('Error checking API availability:', error);
+//     return false;
+//   }
+// }
+
+// export async function GET(request: NextRequest) {
+//   const apiAvailability = await checkApiAvailability();
+
+//   if (apiAvailability) {
+//     return new NextResponse(JSON.stringify({
+//       status: 'Ok'
+//     }));
+//   } else {
+//     return new NextResponse(JSON.stringify({
+//       status: 'Error',
+//       message: 'Unable to connect to the API'
+//     }));
+//   }
+// }
+
+
+async function checkApiAvailability() {
   const pool = new Pool({ connectionString: process.env.NEON_DATABASE_URL });
 
   try {
     await pool.connect();
-    return  new NextResponse(JSON.stringify({
-      status: 'Ok'
-    }));
+    return true;
   } catch (error) {
     console.error('Error checking API availability:', error);
     return false;
   }
 }
+export async function GET(request: NextRequest) {
+  const apiAvailability = await checkApiAvailability();
 
+  if (apiAvailability) {
+    return new NextResponse(JSON.stringify({
+      status: 'Ok'
+    }));
+  } else {
+    return new NextResponse(JSON.stringify({
+      status: 'Error',
+      message: 'Unable to connect to the API'
+    }), { status: 500 });
+  }
+}
