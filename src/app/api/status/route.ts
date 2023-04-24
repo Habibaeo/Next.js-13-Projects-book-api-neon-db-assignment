7ce9759ae83835de7e577361102d314c92a50c40
Habib ur Rehman <habibaeo123@gmail.com>
2023-04-24 21:33:56 +0500

    status code updated

--- src/app/api/status/route.ts
+++ src/app/api/status/route.ts
@@ -1,30 +1,18 @@
 import { NextResponse, NextRequest } from 'next/server';
 import { Pool } from '@neondatabase/serverless';
 
+
 async function checkApiAvailability() {
   const pool = new Pool({ connectionString: process.env.NEON_DATABASE_URL });
 
   try {
-    const { rows } = await pool.query('SELECT NOW()');
-    return true;
+    await pool.connect();
+    return  new NextResponse(JSON.stringify({
+      status: 'Ok'
+    }));
   } catch (error) {
     console.error('Error checking API availability:', error);
     return false;
-  }
-}
-
-export async function GET(request: NextRequest) {
-  const apiAvailability = await checkApiAvailability();
-
-  if (apiAvailability) {
-    return new NextResponse(JSON.stringify({
-      status: 'Ok'
-    }));
-  } else {
-    return new NextResponse(JSON.stringify({
-      status: 'Error',
-      message: 'Unable to connect to the API'
-    }));
   }
 }
 
