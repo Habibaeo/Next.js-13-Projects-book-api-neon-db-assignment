fa88406d04752aba85992a50fb250b04ece14ae5
Habib ur Rehman <habibaeo123@gmail.com>
2023-04-24 19:00:41 +0500

    orderlist route created

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -39,60 +39,6 @@ export async function POST(request: NextRequest, { params }: {
 
   return new NextResponse(JSON.stringify({ message: 'Order successfully placed' }));
 }
-
-
-
-// Orders retrieval
-
-interface Book {
-  id: number;
-  name: string;
-  price: number;
-}
-
-export async function GET(request: NextRequest) {
-  try {
-    const pool = new Pool({ connectionString: process.env.NEON_DATABASE_URL });
-
-    // Retrieve all orders from the orders table
-    const { rows: orders } = await pool.query<Order>('SELECT * FROM orders');
-
-    // If no orders found, return an error response
-    if (orders.length === 0) {
-      return new NextResponse(JSON.stringify({ error: 'No orders found' }), { status: 404 });
-    }
-
-    // Retrieve book information for each order
-    const bookIds = [...new Set(orders.map(order => order.bookId))];
-    const { rows: books } = await pool.query<Book>('SELECT * FROM books WHERE id IN (' + bookIds.join(',') + ')');
-
-    // If no books found for the orders, return an error response
-    if (books.length === 0) {
-      return new NextResponse(JSON.stringify({ error: 'No books found for the orders' }), { status: 404 });
-    }
-
-    // Join the order and book information
-    const orderData = orders.map(order => {
-      const book = books.find(book => book.id === order.bookId);
-      const price = book ? book.price : 0;
-      const total = order.quantity * price;
-
-      return {
-        id: order.id,
-        bookName: book ? book.name : '',
-        quantity: order.quantity,
-        pricePerUnit: price,
-        totalPrice: total
-      };
-    });
-
-    return new NextResponse(JSON.stringify(orderData));
-  } catch (error) {
-    console.error(error);
-    return new NextResponse(JSON.stringify({ error: 'Internal Server Error' }), { status: 500 });
-  }
-}
-
 export const runtime = 'edge';
 
 
